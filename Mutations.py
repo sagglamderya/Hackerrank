0a3a037c65305ac790860c1ae1415e7775959750
Derya <104157454+sagglamderya@users.noreply.github.com>
2022-06-06 23:39:28 +0300

    Mutations

--- Mutations.py
+++ Mutations.py
@@ -1,50 +1,17 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "name": "Untitled0.ipynb",
-      "provenance": [],
-      "authorship_tag": "ABX9TyPuq5/T43SpX1Zkb+Xdc9Yh"
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "code",
-      "execution_count": null,
-      "metadata": {
-        "id": "oy_P166W1zW6"
-      },
-      "outputs": [],
-      "source": [
-        ""
-      ]
-    },
-    {
-      "cell_type": "code",
-      "source": [
-        "def mutate_string(string, position, character):\n",
-        "    s_new=s[:int(i)]+str(c)+s[int(i)+1:]\n",
-        "    return s_new\n",
-        "\n",
-        "if __name__ == '__main__':\n",
-        "    s = input()\n",
-        "    i, c = input().split()\n",
-        "    s_new = mutate_string(s, int(i), c)\n",
-        "    print(s_new)"
-      ],
-      "metadata": {
-        "id": "gbQ8sZdW10AA"
-      },
-      "execution_count": null,
-      "outputs": []
-    }
-  ]
-}
+#Task
+#Read a given string, change the character at a given index and then print the modified string.
+#Function Description
+#Complete the mutate_string function in the editor below.
+#mutate_string has the following parameters:
+#string string: the string to change
+#int position: the index to insert the character at
+#string character: the character to insert
+def mutate_string(string, position, character):
+    s_new=s[:int(i)]+str(c)+s[int(i)+1:]
+    return s_new
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
