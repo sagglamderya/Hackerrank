29624eb44c93e8241742e221bb099b044cdd6856
Derya <104157454+sagglamderya@users.noreply.github.com>
2022-06-06 23:22:40 +0300

    String Split and Join

--- String_Split_and_Join.py
+++ String_Split_and_Join.py
@@ -1,48 +1,9 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "name": "Untitled0.ipynb",
-      "provenance": [],
-      "authorship_tag": "ABX9TyPJmJlkVmcYKSIoTQYFDhCe"
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "code",
-      "execution_count": null,
-      "metadata": {
-        "id": "oy_P166W1zW6"
-      },
-      "outputs": [],
-      "source": [
-        ""
-      ]
-    },
-    {
-      "cell_type": "code",
-      "source": [
-        "def split_and_join(line):\n",
-        "    line=\"-\".join(line.split(\" \"))\n",
-        "    return line\n",
-        "if __name__ == '__main__':\n",
-        "    line = input()\n",
-        "    result = split_and_join(line)\n",
-        "    print(result)"
-      ],
-      "metadata": {
-        "id": "gbQ8sZdW10AA"
-      },
-      "execution_count": null,
-      "outputs": []
-    }
-  ]
-}
+#You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+
+def split_and_join(line):
+    line="-".join(line.split(" "))
+    return line
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
